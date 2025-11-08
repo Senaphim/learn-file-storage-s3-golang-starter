aad89b06627050cdb368009d99f2c2d419747adc
senaphim <senaphim@gmail.com>
2025-11-08 14:13:27 +0000

    Removed all old methods of handling video thumbnails and created final way of doing it

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -41,17 +46,28 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	media, mediaType, err := r.FormFile("thumbnail")
+	media, header, err := r.FormFile("thumbnail")
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't extract media", err)
 		return
 	}
 
-	mediaBytes, err := io.ReadAll(media)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't convert image to bytes", err)
+	mediaType := header.Header.Get("Content-Type")
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Malformed request header", nil)
 		return
 	}
+
+	allowedTypes := map[string]int{
+		"image/jpg": 0,
+		"image/png": 0,
+	}
+	if _, ok := allowedTypes[mediaType]; !ok {
+		respondWithError(w, http.StatusBadRequest, "Incorrect media type", nil)
+		return
+	}
+
+	mediaType = strings.Split(mediaType, "/")[1]
 
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -69,13 +85,32 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	videoThumbnail := thumbnail{data: mediaBytes, mediaType: mediaType.Filename}
-	videoThumbnails[videoID] = videoThumbnail
+	byteSlice := make([]byte, 32)
+	_, err = rand.Read(byteSlice)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error generating byte slice", err)
+	}
+	randomString := base64.RawURLEncoding.EncodeToString(byteSlice)
+	thumbnailFilename := fmt.Sprintf("%s.%s", randomString, mediaType)
+	thumbnailFilepath := filepath.Join(cfg.assetsRoot, thumbnailFilename)
 
-	thumbnailUrl := fmt.Sprintf("http://localhost:8091/api/thumbnails/%s", videoID)
-	video.ThumbnailURL = &thumbnailUrl
+	file, err := os.Create(thumbnailFilepath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving thumbnail", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, media)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving thumbnail", err)
+		return
+	}
+
+	dataUrl := fmt.Sprintf("http://localhost:8091/assets/%s", thumbnailFilename)
+	video.ThumbnailURL = &dataUrl
 	if err = cfg.db.UpdateVideo(video); err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Failed to update video thumbnail", err)
+		respondWithError(w, http.StatusInternalServerError, "Error updating video", err)
 		return
 	}
 
